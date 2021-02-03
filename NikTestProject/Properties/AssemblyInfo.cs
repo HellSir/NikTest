673afb258331459798f10c421b3bc6a75d7f9667
Vladimir Navalnyi <ggbet@mail.ru>
2021-02-03 13:06:18 +0200

    Добавьте файлы проекта.

---
+++ NikTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NikTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NikTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f35f002d-5ef5-4c4d-8924-0dc18b1da2cf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NikTestProject/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static NikTest.Program;
+
+namespace NikTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] mass = { 12, 55, 45, 78 };
+            int[] massCurr = { 12, 45, 55, 78 };
+
+            IntComparer myComparer = new IntComparer();
+            Heap<int> heap = new Heap<int>(mass, myComparer);
+            heap.HeapSort();
+
+            int[] massEnd = heap._array;
+
+            bool equal_Curr = massCurr.SequenceEqual(massEnd);
+
+            Assert.AreEqual(true, equal_Curr);
+        }
+    }
+}
